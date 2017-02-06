b833ca737bea5b44345997f1e72eeefdce08f01d
tyronepost <tyrone.post.a@gmail.com>
2017-02-05 19:50:26 -0500

    adding index controller, template and thymeleaf dependency

---
+++ src/main/java/hello/IndexController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by tyronepost on 2/5/17.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String hello() {
+        return "index";
+    }
+}
